44fb04a83d9636211ac58617d81c8f27b065f91e
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-11-29 00:00:21 +0500

    task1

--- Tyuiu.BaimuhametovaMD.Sprint6.Task1.V27.Lib/DataService.cs
+++ Tyuiu.BaimuhametovaMD.Sprint6.Task1.V27.Lib/DataService.cs
@@ -9,8 +9,8 @@ namespace Tyuiu.BaimuhametovaMD.Sprint6.Task1.V27.Lib
             double[] res = new double[stopValue  -  startValue + 1];
             for (int i = startValue; i <= stopValue; i++)
             {
-                if (2 * i - 2 === 0) res[i] = 0;
-                else res[i + 5] = Math.Round(4 - 2 * i + ((2 + Math.Cos(i)) / Convert.ToDouble(2 * i - 2)), 2);
+                if (2 * i - 2 == 0) res[i] = 0;
+                else res[i + 5] = Math.Round((4 - 2 * i + ((2 + Math.Cos(i)) / (2 * (double)i - 2))), 2);
             }
             return res;
         }
