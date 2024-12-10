875a06f84073a382feea5cc6d9133910ac315669
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-12-10 22:54:09 +0500

    task6

---
+++ Tyuiu.BaimuhametovaMD.Sprint6.Task6.V23/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BaimuhametovaMD.Sprint6.Task6.V23
+{
+    public partial class FormMain: Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
